228c065991c0a774b122ca8719f62ecb2939e167
pradkris <pradkris07@gmail.com>
2025-11-10 21:55:03 +0530

    created the environment using uv

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pradeepkumar",
+    author_email="pradkris07@gmail.com",
+    packages=find_packages()
+)
