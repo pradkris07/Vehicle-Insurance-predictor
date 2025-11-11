df1d50572b5d6e79bb00a690c7457ad87edac0b6
pradkris <pradkris07@gmail.com>
2025-11-11 11:31:25 +0530

    added data transformation part to the program

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -17,3 +17,30 @@ def read_yaml_file(file_path: str) -> dict:
 
     except Exception as e:
         raise MyException(e, sys) from e
+        
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise MyException(e, sys) from e
+        
+def save_object(file_path: str, obj: object) -> None:
+    logging.info("Entered the save_object method of utils")
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+        logging.info("Exited the save_object method of utils")
+
+    except Exception as e:
+        raise MyException(e, sys) from e
